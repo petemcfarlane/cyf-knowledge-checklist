7dd00d37e51b3af1bfdae2f48b64088bb7fdc87f
csfilipinyi <csaba.filipinyi@gmail.com>
2020-10-09 21:03:34 +0100

    mongodb removed, pg added

--- server/db.js
+++ server/db.js
@@ -1,6 +1,1 @@
-import { MongoClient } from "mongodb";
-
-const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/cyf";
-const configuration = { useNewUrlParser: true, useUnifiedTopology: true };
-
-export const getClient = () => new MongoClient(dbUrl, configuration);
+const { Client } = require('pg')
