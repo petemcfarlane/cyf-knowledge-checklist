35ab38fdbf35cae57c43d3621237531cc6b5097f
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-10-15 13:51:47 +0100

    user sign up insert works in updated schema with encrypted passwords

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -5,27 +5,26 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 
 
-
+ 
   router.post("/register", async (req, res) => {
-    const { firstName, lastName, userRole, userEmail, userSlack, userPassword, userGithub, userClassId } = req.body;
+    const { firstName, lastName, userRole, userEmail, userSlack, userPassword, userGithub, userClassId ,username,userPhone,cyfCity,userDateOfBirth} = req.body;
     try {
-      //destructure req.body
+      
       const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
         userEmail
       ]);
        if (user.rows.length !== 0) {
        return res.status(401).json("User already exist!");
       }
-      const saltRound = 10;
-      const salt = await bcrypt.genSalt(saltRound);
-      const bcryptPassword = bcrypt.hash(userPassword,salt);
+      
+      const salt = await bcrypt.genSalt(10);
+      const bcryptPassword = await bcrypt.hash(userPassword, salt);
 
-      const newUser = await pool.query(
-        'INSERT INTO users (first_name, last_name, user_role,user_email,user_slack,user_password,user_github,' +
-        'class_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING * ',
-        [firstName, lastName, userRole, userEmail, userSlack, bcryptPassword, userGithub, userClassId]
-  
-      );
+      let newUser = await pool.query(
+        'INSERT INTO users (first_name, last_name, user_role,user_email,user_slack,user_password,user_github,class_id,username,user_phone,cyf_city,user_date_of_birth)'+
+        ' VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) returning *' ,
+        [firstName, lastName, userRole, userEmail, userSlack, bcryptPassword, userGithub, userClassId, username, userPhone, cyfCity, userDateOfBirth]
+        );
       res.json(newUser);
     }
     
