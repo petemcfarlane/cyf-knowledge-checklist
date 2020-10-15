cb45d36efd8eb47514a6029c58d3fb79ece6afb4
F Nawal Abdul Careem <nawal@fs-macbook-air.home>
2020-10-15 20:54:31 +0100

    corrected conflict error on login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
+import {Jumbotron,Container, Form, Col ,Row } from 'react-bootstrap'
 
-import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import { Link } from "react-router-dom";
 export default function LoginForm() {
     const [input, setInput]= useState({
         email: "",
@@ -8,20 +10,77 @@ export default function LoginForm() {
     })
 
    function handleChange(event){
-       event.prevendDefault();
-       setInput(event.target.value);
+       let updateInput = {
+           ...input,
+           [event.target.name]: event.target.value
+       }
+        console.log(updateInput)
+       setInput(updateInput);
    }
-
+   function handleSubmit(event){
+    event.prevendDefault();
+    setInput({
+        email: "",
+        password: ""
+           })
+    }
     return (
 
-        <div>
-            <h1>Welcome to our page</h1>
-            <label>Email</label>
-            <input type="email" placeholder="Enter email" name="email" onChange={handleChange} value={input.email} />
-            <label>Password</label>
-            <input type="password" placeholder="Enter email" name="password" onChange={handleChange} value={input.password} />
-            <Link to = "/Skills">< button type="button" class="btn btn-secondary">Login</button></Link>
-            <Link to="/signup">signup</Link>
-        </div>
+        //     <form onSubmit={handleSubmit}>
+        // <div>
+        //     <h1>Welcome to our page</h1>
+        //     <label>Email</label>
+        //     <input type="email" placeholder="Enter email" name="email" onChange={handleChange} value={input.email} />
+        //     <label>Password</label>
+        //     <input type="password" placeholder="Enter email" name="password" onChange={handleChange} value={input.password} />
+        //     <input type="submit" value="login" />
+        //     <Link to="/signup">signup</Link>
+        // </div>
+        //  </form>
+
+        <Jumbotron fluid>
+<Container>
+
+     <Form onSubmit={handleSubmit}>
+            
+  
+          
+          <Col as={Row} sm={{ span: 5, offset: 5 }}>
+            <label  size="lg" controlId="formBasicEmail" >Email</label>
+            </Col>
+          <Col as={Row} sm={{ span: 5, offset: 5 }}>
+             <input  
+             type="email" 
+             placeholder="Enter email" 
+             name="email" 
+             onChange={handleChange} 
+             value={input.email} 
+             />
+             </Col>
+             <br />
+            <Col  sm={{ span: 5, offset: 5 }}>
+               <label  size="lg" as={Row} controlId="formHorizontalPassword" >Password</label></Col>
+            <Col as={Row} sm={{ span: 5, offset: 5 }}>
+               <input 
+               type="password" 
+               placeholder="Enter password" 
+               name="password" 
+               onChange={handleChange} 
+               value={input.password} />
+               </Col>
+               <br />
+            <Col as={Row} sm={{ span: 5, offset: 5 }}>
+              <Link to = "/Skills">< button type="button" variant="secondary" size="lg" p-2 active >Login</button></Link>
+              </Col>
+            <Col as={Row} sm={{ span: 5, offset: 5 }}>
+               <Link to="/signup">signup</Link> 
+               </Col>
+        </Form>    
+
+
+</Container>
+</Jumbotron>
+
+
     )
 }
